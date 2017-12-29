3a9df64f6a49aad26b2b50f8b9e8d3897db649cf
Jose Rodrigues <ze.pedro.rodrigues@gmail.com>
2017-12-29 14:51:20 +0000

    add install info

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = 8080;
+var port = 3000;
 
 app.use(express.static(__dirname + '/public')); // set the static files location
 app.listen(port);
